b22bf3ff8ded4d7e542f7309edb5bafea595657d
Gabriel Silva <gabrielssantana41@gmail.com>
2025-05-25 17:58:03 -0300

    atualização homepage

---
+++ src/app/pedidos/page.tsx
@@ -0,0 +1,5 @@
+export default function Pedidos() {
+    return(
+        <div className="text-black">oi</div>
+    );
+}
--- src/components/NavBottom.tsx
+++ src/components/NavBottom.tsx
@@ -1,24 +1,31 @@
 'use client';
 import { Home, ShoppingBag, User } from 'lucide-react';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function NavBottom() {
   const [selected, setSelected] = useState('inicio');
+  const router = useRouter();
 
   const menuItems = [
-    { id: 'inicio', label: 'Início', icon: Home },
-    { id: 'pedidos', label: 'Pedidos', icon: ShoppingBag },
-    { id: 'perfil', label: 'Perfil', icon: User },
+    { id: 'Cardápio', label: 'Cardápio', icon: Home, href: '/' },
+    { id: 'Pontos', label: 'Pontos', icon: ShoppingBag, href: '/pag-pontos' },
+    { id: 'perfil', label: 'Perfil', icon: User, href: '/perfil' },
   ];
 
+  const handleClick = (id: string, href: string) => {
+    setSelected(id);
+    router.push(href);
+  };
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 w-full bg-white text-white flex justify-around items-center h-16 border-t border-gray-800 z-50">
-      {menuItems.map(({ id, label, icon: Icon }) => (
+    <div className="fixed bottom-0 left-0 right-0 w-full bg-amber-950 text-white flex justify-around items-center h-16 border-t border-gray-800 z-50">
+      {menuItems.map(({ id, label, icon: Icon, href }) => (
         <button
           key={id}
-          onClick={() => setSelected(id)}
+          onClick={() => handleClick(id, href)}
           className={`flex flex-col items-center text-xs focus:outline-none cursor-pointer ${
-            selected === id ? 'text-blue-500' : 'text-black'
+            selected === id ? 'text-orange-700' : 'text-white'
           }`}
         >
           <Icon size={24} />
