d1f2ea11b14f24a4bda9f6b4b5c36b61cf45d512
Lucas <lucasommarins@gmail.com>
2025-09-23 15:50:33 -0300

    Corrige erros de load da page docs

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import SwaggerUI from "swagger-ui-react";
+import { useEffect, useState } from "react";
 import "swagger-ui-react/swagger-ui.css";
 
 export default function DocsPage() {
+  const [SwaggerUI, setSwaggerUI] = useState<any>(null);
+
+  useEffect(() => {
+    import("swagger-ui-react").then(mod => setSwaggerUI(() => mod.default));
+  }, []);
+
+  if (!SwaggerUI) return <p>Carregando documentação...</p>;
+
   return <SwaggerUI url="/api/docs" />;
 }
